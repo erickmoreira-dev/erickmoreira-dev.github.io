09efd02833ead8a1a0d44958c1efdf2f7cc10dba
heryckmp <seu-email@exemplo.com>
2025-03-07 20:08:24 -0300

    Implementação do easter egg hack com vídeo e correções na animação do GameBoy 3D

--- src/gameboy.js
+++ src/gameboy.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
-import WebGL from '/src/webgl';
+import gsap from 'gsap';
 
 class GameBoy {
   constructor() {
